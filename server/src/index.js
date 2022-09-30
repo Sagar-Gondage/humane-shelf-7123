5a7171a158591a629c75961450c3ebbab217a6c6
Ravi Nishad <97040275+ravinishadcoder@users.noreply.github.com>
2022-09-30 09:33:46 +0530

    cors added

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,11 @@
 const express=require('express')
 const dotenv = require("dotenv");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const ProductRouter = require('./router/product.route');
 dotenv.config();
 const app=express()
+app.use(cors())
 const mongodb_url = process.env.MONGO_URL||"mongodb://localhost:27017/tata1mg"
 const PORT = process.env.PORT
 app.use(express.urlencoded({extended:true}))
