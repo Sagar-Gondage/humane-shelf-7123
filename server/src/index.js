d792cd941a28b7486fd066b51b9b5632d89d939b
Ravi Nishad <97040275+ravinishadcoder@users.noreply.github.com>
2022-09-30 21:53:10 +0530

    user route added

--- server/src/index.js
+++ server/src/index.js
@@ -1,24 +1,25 @@
-const express=require('express')
+const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const ProductRouter = require('./router/product.route');
+const ProductRouter = require("./router/product.route");
+const UserRouter = require("./router/user.route");
 dotenv.config();
-const app=express()
-app.use(cors())
-const mongodb_url = process.env.MONGO_URL||"mongodb://localhost:27017/tata1mg"
-const PORT = process.env.PORT
-app.use(express.urlencoded({extended:true}))
-app.use(express.json())
+const app = express();
+app.use(cors());
+const mongodb_url = process.env.MONGO_URL || "mongodb://localhost:27017/tata1mg";
+const PORT = process.env.PORT;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //Product Route
-app.use("/products",ProductRouter)
-
-
+app.use("/products", ProductRouter);
+//User Route
+app.use("/user",UserRouter);
 
 mongoose.connect(mongodb_url).then(() => {
-   
-    app.listen(PORT, () => {
-      console.log("server is started on port " + PORT);
-    });
+ 
+  app.listen(PORT, () => {
+    console.log("server is started on port " + PORT);
   });
+});
---
+++ server/src/model/user.model.js
@@ -0,0 +1,16 @@
+
+
+const { Schema, model } = require("mongoose");
+
+const UserSchema = Schema({
+  firstName: String,
+  lastName: String,
+  email: String,
+  mobile: String,
+  token: String,
+  cart:{type:Array,default:[]}
+});
+
+const UserModel = model("user", UserSchema);
+
+module.exports = UserModel;
